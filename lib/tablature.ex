38d6cbd6ad986d1fdc39c5ece1da4eb6a7536ea1
MiguelMonroy <A01739891@tec.mx>
2026-05-05 21:26:38 -0600

    Parte 1

--- lib/tablature.ex
+++ lib/tablature.ex
@@ -1,8 +1,22 @@
 defmodule Tablature do
   def parse(tab) do
-    Regex.scan(~r/\d+/, tab)
-    |> List.flatten()
-    |> Enum.map(fn n -> "B" <> n end)
+    tab
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+    |> Enum.zip()
+    |> Enum.flat_map(&Tuple.to_list/1)
+    |> Enum.reject(&is_nil/1)
     |> Enum.join(" ")
+  end
+
+  defp parse_line(line) do
+    [string, notes] = String.split(line, "|", parts: 2)
+
+    Regex.scan(~r/\d+|-/, notes)
+    |> List.flatten()
+    |> Enum.map(fn
+      "-" -> nil
+      note -> string <> note
+    end)
   end
 end
